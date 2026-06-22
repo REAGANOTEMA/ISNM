bacf482a11f51e4d75be6a648a3b5b31078c7535
REAGANOTEMA <reaganotema2022@gmail.com>
2026-06-22 13:19:07 -0700

    initial changes

--- sql/staffs/igangaschoolofl_staffs_db.sql
+++ sql/staffs/igangaschoolofl_staffs_db.sql
@@ -1,15 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 22, 2026 at 08:28 PM
--- Server version: 8.0.45
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
